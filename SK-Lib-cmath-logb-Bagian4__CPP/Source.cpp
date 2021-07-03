205568d74a85d25ffae26d785895cd37e8185f32
Rizky Khapidsyah <Rizky Khapidsyah@RIZKYKHAPIDSYAH>
2021-07-03 08:31:31 +0700

    Add project files.

---
+++ SK-Lib-cmath-logb-Bagian4__CPP/Source.cpp
@@ -0,0 +1,17 @@
+
+#include <cmath>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	double hasil;
+	int x = 0;
+
+	hasil = logb(x);
+	cout << "logb(" << x << ") = " << "log(|" << x << "|) = " << hasil << endl;
+
+	_getch();
+	return 0;
+}
